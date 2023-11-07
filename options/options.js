2f4089ba51a418d3197115f48cccbce9f8898a6c
Jared Merrill <jm101designs@gmail.com>
2023-11-06 19:06:22 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "beat-of-life-44362",
   url: "https://beat-of-life-44362.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,33 +1,23 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import { Pressable } from "react-native";
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView, StyleSheet } from "react-native";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
+          <Pressable><Image style={styles.logo} source={require("./AdobeStock_613652891.jpeg")} resizeMode="stretch" /></Pressable>
+          <Text style={styles.text}>{"Do you dance to the beat of life? \nLet music guide you to find your person!          "}</Text>
         </View>
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+      <View style={styles.lgzRtWbg}><Text style={styles.RcmFByLM}>{"Sign Up!"}</Text></View></ScrollView>
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F8F8FC",
+    backgroundColor: "#271c69",
     height: "100%"
   },
   scrollView: {
@@ -40,17 +30,21 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   logo: {
-    height: 180,
-    width: 180,
+    height: 80,
+    width: 319,
     padding: 40,
     borderRadius: 30,
-    margin: 40
+    margin: 40,
+    flexDirection: "row",
+    flex: 1,
+    flexWrap: "wrap"
   },
   text: {
     textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
+    fontSize: 23,
+    color: "#04f94d",
+    fontWeight: 700,
+    fontFamily: "Playfair Display"
   },
   footer: {
     textAlign: "center",
@@ -58,7 +52,33 @@ const styles = StyleSheet.create({
     color: "#828AB0",
     fontWeight: 700,
     marginBottom: 20
+  },
+  lgzRtWbg: {
+    height: 35,
+    width: 126,
+    backgroundColor: "#981795",
+    borderRadius: 40,
+    color: "#777777",
+    borderColor: "#0cf915",
+    borderWidth: 3,
+    position: "absolute",
+    top: 529,
+    left: 107
+  },
+  RcmFByLM: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 21,
+    borderRadius: 0,
+    fontFamily: "Playfair Display",
+    position: "absolute",
+    top: 6,
+    left: 13,
+    color: "#08ff12",
+    opacity: 1,
+    fontWeight: "700",
+    textAlign: "center"
   }
-})
-
-export default WelcomeScreen
+});
+export default WelcomeScreen;
